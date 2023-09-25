c487990c55101501d573d1fa4f908c47efa102c5
Rahul Kumar <rajabhi78580@gmail.com>
2023-09-25 13:59:33 +0530

    first commit

---
+++ app/src/main/java/com/example/videoview/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.videoview
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    // declaring a null variable for VideoView
+    var simpleVideoView: VideoView? = null
+
+    // declaring a null variable for MediaController
+    var mediaControls: MediaController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // assigning id of VideoView from
+        // activity_main.xml layout file
+        simpleVideoView = findViewById<View>(R.id.simpleVideoView) as VideoView
+
+        if (mediaControls == null) {
+            // creating an object of media controller class
+            mediaControls = MediaController(this)
+
+            // set the anchor view for the video view
+            mediaControls!!.setAnchorView(this.simpleVideoView)
+        }
+
+        // set the media controller for video view
+        simpleVideoView!!.setMediaController(mediaControls)
+
+        // set the absolute path of the video file which is going to be played
+        simpleVideoView!!.setVideoURI(Uri.parse("https://firebasestorage.googleapis.com/v0/b/fir-databaseauth-10ba2.appspot.com/o/videoo.mp4?alt=media&token=858dde55-f85e-4ec8-8d5d-3a13fb8007dd"))
+
+        simpleVideoView!!.requestFocus()
+
+        // starting the video
+        simpleVideoView!!.start()
+
+        // display a toast message
+        // after the video is completed
+        simpleVideoView!!.setOnCompletionListener {
+            Toast.makeText(applicationContext, "Video completed",
+                Toast.LENGTH_LONG).show()
+            true
+        }
+        simpleVideoView!!.setOnErrorListener { mp, what, extra ->
+            Toast.makeText(applicationContext, "An Error Occurred " +
+                    "While Playing Video !!!", Toast.LENGTH_LONG).show()
+            false
+        }
+    }
+}
